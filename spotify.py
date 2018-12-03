c9eb26d44c8df4f687ab5b0e250e7bafb8c507d1
Simon Zhang <simonczhang@gmail.com>
2018-12-02 20:27:50 -0600

    create instance of spotify api client fully authorized

--- spotify.py
+++ spotify.py
@@ -5,12 +5,44 @@ Created on Sun Dec  2 18:59:55 2018
 @author: simon
 """
 
-import sys
 import spotipy
+import spotipy.util as util
 from pprint import pprint
 import json
 
-with open('secret.json') as f:
-    data = json.load(f)
+def make_spotify():
+    with open('secret.json') as f:
+        data = json.load(f)
+        
+    scope = data['scopes']
+    username = data['username']
+    client_id = data['client_id']
+    client_secret = data['client_secret']
+    redirect_uri = data['redirect_uri']
+    
+    token = util.prompt_for_user_token(
+            username, 
+            scope, 
+            client_id=client_id, 
+            client_secret=client_secret,
+            redirect_uri=redirect_uri
+            )
+    
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print("Couldn't authorize")
+        
+    return sp
     
 
+
+if __name__ == '__main__':
+    sp = make_spotify()
+    
+    
+    top_artists = sp.current_user_top_artists()
+    top_tracks = sp.current_user_top_artists()
+    
+
+    
