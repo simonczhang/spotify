3f9de63d5760efdaa58ac2b088a66cc47362566c
Simon Zhang <simonczhang@gmail.com>
2018-12-02 19:49:44 -0600

    read in data from secret.json

--- spotify.py
+++ spotify.py
@@ -5,3 +5,12 @@ Created on Sun Dec  2 18:59:55 2018
 @author: simon
 """
 
+import sys
+import spotipy
+from pprint import pprint
+import json
+
+with open('secret.json') as f:
+    data = json.load(f)
+    
+
